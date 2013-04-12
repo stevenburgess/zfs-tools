f49b711f6eefe639c6099289dd15a5f6c143ef36
Steven Burgess <sburgess@dattobackup.com>
2013-04-11 22:53:52 -0400

    comments and refactors for clarity

--- zfs-arc.py
+++ zfs-arc.py
@@ -1,30 +1,38 @@
 import subprocess
 
-
+"""
+system memory portion
+"""
 output = subprocess.check_output(["free", "-g"])
-
 lines = str.splitlines(output)
-
 memoryline = lines[1]
-
 splitLine = str.split(memoryline)
-
 totalSystemRamInGb = splitLine[1]
-
 usedSystemRamInGb = splitLine[2]
-
 freeSystemRamInGb = splitLine[3]
 
 print "Total System Memory\t" + totalSystemRamInGb + "\tGB"
 print "Used System Memory\t" + usedSystemRamInGb + "\tGB"
 print "Free System Memory\t" + freeSystemRamInGb + "\tGB"
-
+#a seperator
 print "============"
 
+"""
+zfs adaptive replacment cache stats are posted via kstat
+to a file in proc, read in that file
+"""
 arcFile = open('/proc/spl/kstat/zfs/arcstats','r')
 
-allLines = arcFile.readlines();
+arcFileLines = arcFile.readlines();
+#fill the contentDictionary with key/value pairs
+contentDictionary = {}
+for line in arcFileLines:
+        splitLine = str.split(line)
+        contentDictionary[splitLine[0]] = splitLine[2]
 
+#dictionary of the arc stats we care about for this program
+#the key is a value we care about, the value is a description
+#of what that value means
 importantValues = {
 'size' : 'Current Arc Size',
 'c_min' : 'Minimum Arc Size',
@@ -35,10 +43,6 @@ importantValues = {
 'arc_meta_max' : 'The Largest Arc Meta Has Been(?)',
 'l2_size' : 'Size of L2 cache'
 }
-contentDictionary = {}
-for line in allLines:
-        splitLine = str.split(line)
-        contentDictionary[splitLine[0]] = splitLine[2]
 
 for value, description in importantValues.iteritems():
         sizeInBytes = contentDictionary[value]
