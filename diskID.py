62361a4fe7ff4c9c275ec3a2891a55ecc7614e0b
Steven Burgess <sburgess@dattobackup.com>
2013-07-18 23:22:04 -0400

    Adds the zpool section
    Now this script can be used to tell what disks are connected to zfs pools and
    what disks are avail.

--- diskID.py
+++ diskID.py
@@ -4,7 +4,10 @@ import subprocess
 #I choose ata-
 prefixOfChoice = 'ata-'
 
-diskList = subprocess.check_output(['ls', '-l', '/dev/disk/by-id/']);
+#a list containing all the disks connected to this machine
+allDisks = []
+
+diskList = subprocess.check_output(['ls', '-l', '/dev/disk/by-id/'])
 for line in diskList.split('\n'):
 	#exclude partition lines
 	if 'part' in line:
@@ -13,5 +16,26 @@ for line in diskList.split('\n'):
 		brokenLine = line.split(' ')
 		for part in brokenLine:
 			if prefixOfChoice in part:
-				print part
+				allDisks.append(part)
 
+#a list of all the disks being used by ZFS
+zfsDisks = []
+
+#a list that will only contain disks not used by ZFS in the end
+nonZfsDisks = list(allDisks)
+
+zpoolStatus = subprocess.check_output(['zpool', 'status'])
+for zpoolLine in zpoolStatus.split('\n'):
+	if prefixOfChoice in zpoolLine:
+		diskID = zpoolLine.split(' ')[4]
+		zfsDisks.append(diskID)
+		nonZfsDisks.remove(diskID)
+
+print "Disks used by ZFS:"
+for disk in zfsDisks:
+	print disk
+
+print '###############'
+print "Disks not used by ZFS:"
+for disk in nonZfsDisks:
+	print disk
