3b068828bc20665036a6ccee70ef4438d2331a62
Steven Burgess <sburgess@dattobackup.com>
2013-08-02 17:50:41 -0400

    adds the scisi ID to the output
    While I am not a fan of the /dev/sd* names, especially when it comes to
    zpool creation, some linux services work much better with these IDs.
    
    This updates the script to print out both the /dev/disk/by-id entries
    and the /dev/sd* entries.

--- diskID.py
+++ diskID.py
@@ -5,7 +5,7 @@ import subprocess
 prefixOfChoice = 'ata-'
 
 #a list containing all the disks connected to this machine
-allDisks = []
+allDisks = dict()
 
 diskList = subprocess.check_output(['ls', '-l', '/dev/disk/by-id/'])
 for line in diskList.split('\n'):
@@ -14,9 +14,14 @@ for line in diskList.split('\n'):
 		continue	
 	if prefixOfChoice in line:
 		brokenLine = line.split(' ')
+		currentDisk = dict()
+		#print brokenLine
 		for part in brokenLine:
 			if prefixOfChoice in part:
-				allDisks.append(part)
+				currentDisk['id'] = part
+			elif 'sd' in part:
+				currentDisk['sd'] = part.replace('../../', '')
+		allDisks[currentDisk['id']] = currentDisk
 
 #a list of all the disks being used by ZFS
 zfsDisks = []
@@ -31,11 +36,19 @@ for zpoolLine in zpoolStatus.split('\n'):
 		zfsDisks.append(diskID)
 		nonZfsDisks.remove(diskID)
 
+#this is the number of spaces to pad the ID number.  It should be a few
+#characters longer than the longest disk ID.
+padNumber = 45
+
 print "Disks used by ZFS:"
 for disk in zfsDisks:
-	print disk
+	diskID = allDisks[disk]['id']
+	diskName = allDisks[disk]['sd']
+	print diskID.ljust(padNumber) + diskName.rjust(6)
 
 print '###############'
 print "Disks not used by ZFS:"
 for disk in nonZfsDisks:
-	print disk
+	diskID = allDisks[disk]['id']
+	diskName = allDisks[disk]['sd']
+	print diskID.ljust(padNumber) + diskName.rjust(6)
