814998a97bced708fb86284c27a80f36c23a3a94
Steven Burgess <steven.a.burgess@hotmail.com>
2013-10-05 23:16:41 -0400

    Updates for stat.py
    uses the first argument as the file to inspect, and adds more printouts

--- stat.py
+++ stat.py
@@ -1,9 +1,18 @@
 import os
 import pprint
+import sys
 
-stat = os.stat('/var/lib/libvirt/images/ubuntu13desktop/ubuntu13desktop.img')
+#grab the first argument passed
+fileName = sys.argv[1]
+
+stat = os.stat(fileName)
 blocksAllocated = stat.st_blocks
 blockSize = stat.st_blksize
+#the total amount consumed is the block size * blocks allocated
+totalConsumed = blockSize * blocksAllocated
 size = stat.st_size
 
-print "bytes"
+print str(blocksAllocated) + " number of blocks consumed"
+print str(blockSize) + " blockSize"
+print str(totalConsumed) + " real size consumed"
+print str(size) + " total (reported) size"
